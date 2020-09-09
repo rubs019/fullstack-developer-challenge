88e7d4fd8a4b07e1488d14175569453a933dd8b2
Ruben DESERT <ruben.desert@gmail.com>
2020-09-09 12:31:48 -0400

    feat(api): Add cluster application

--- NEWCOMER_NOTES.md
+++ NEWCOMER_NOTES.md
@@ -75,6 +75,11 @@ API
  -> Ajout du rateLimiter ~10mn
  -> Ajout de la fonction Country Breakdown ~20mn
  -> Ajout de la fonction Fake entries cleanup ~5mn
-
+ -> Ajout de la clusterisation de l'application
+    -> Pour le coup je ne suis pas sur de cette solution.
+    -> Le problème étant que je n'ai pas constater de pic particuler au démarrage de l'application
+    -> J'ai donc clusterisé l'application "par défaut" comme on fait sur node.js habituellement
+    -> Néanmoins je ne peux vérifier ma solution à 100%
+    -> Je reste donc cette sceptique concernant cette solution 
     
 
--- api/src/index.js
+++ api/src/index.js
@@ -2,6 +2,8 @@ import compression from 'compression';
 import cors from 'cors';
 import express from 'express';
 import logger from './lib/logger.js';
+import cluster from 'cluster'
+import os from 'os'
 import handleError from './middlewares/handle-error.js';
 import logQuery from './middlewares/log-query.js';
 import postalCodesRouter from './routes/postal-codes.js';
@@ -9,32 +11,48 @@ import locationsRouter from './routes/locations.js';
 import rateLimit from 'express-rate-limit';
 import countriesRouter from "./routes/countries.js";
 
-const port = process.env.PORT || 4000;
+const numCPUs = os.cpus().length;
 
-process.on('unhandledRejection', (reason, promise) => {
-  logger.error('Unhandled Rejection at: Promise ', promise, reason);
-});
+if (cluster.isMaster) {
+  let i = 0;
 
-const app = express();
+  while (i < numCPUs) {
+    cluster.fork();
+    i++;
+  }
 
-app.use(compression());
-app.use(cors());
+  cluster.on("exit", function(worker, code, signal) {
+    console.log("worker " + worker.process.pid + " died");
+  });
+} else {
+  const port = process.env.PORT || 4000;
 
-app.use('*', logQuery);
+  process.on('unhandledRejection', (reason, promise) => {
+    logger.error('Unhandled Rejection at: Promise ', promise, reason);
+  });
 
-/** app.use(rateLimit({
-  windowMs: 60 * 500, // 30 secondes
-  max: 2, // limit each IP to 2 requests per windowMs
-  message:
-      "Too many accounts created from this IP, please try again in 30 seconds"
-}))
-*/
-app.get('/locations', locationsRouter);
-app.get('/postal-codes', postalCodesRouter);
-app.get('/countries', countriesRouter);
+  const app = express();
 
-app.use(handleError);
+  app.use(compression());
+  app.use(cors());
 
-app.listen(port, () => {
-  logger.info(`Express listening on port ${port}`);
-});
+  app.use('*', logQuery);
+
+  app.use(rateLimit({
+    windowMs: 60 * 500, // 30 secondes
+    max: 2, // limit each IP to 2 requests per windowMs
+    message:
+        "Too many accounts created from this IP, please try again in 30 seconds"
+  }))
+
+  app.get('/locations', locationsRouter);
+  app.get('/postal-codes', postalCodesRouter);
+  app.get('/countries', countriesRouter);
+
+  app.use(handleError);
+
+  app.listen(port, () => {
+    logger.info(`Express listening on port ${port}`);
+  });
+
+}
